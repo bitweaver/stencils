3b88dd69cef4baddbfaabe0556152a11b664ce88
laetzer <laetzer@users.sourceforge.net>
2008-09-25 01:12:18 +0000

    stencils listing for index page. that other code just displayed a header

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_stencils/index.php,v 1.3 2008/06/25 22:21:24 spiderr Exp $
+// $Header: /cvsroot/bitweaver/_bit_stencils/index.php,v 1.4 2008/09/25 01:12:18 laetzer Exp $
 // Copyright (c) 2004 bitweaver Stencil
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -13,6 +13,10 @@ $gBitSystem->verifyPackage( 'stencil' );
 // Now check permissions to access this page
 $gBitSystem->verifyPermission( 'p_stencil_read' );
 
+
+
+// doesn't seem to do much -- laetzer 25.09.2008 03:08
+/* 
 if( !isset( $_REQUEST['stencil_id'] ) ) {
 	$_REQUEST['stencil_id'] = $gBitSystem->getConfig( "home_stencil" );
 }
@@ -23,4 +27,20 @@ $gContent->addHit();
 
 // Display the template
 $gBitSystem->display( 'bitpackage:stencil/stencil_display.tpl', tra( 'Stencil' ) , array( 'display_mode' => 'display' ));
+*/
+
+
+
+// pasted from list_stencils.php -- laetzer 25.09.2008 03:09
+// create new stencil object
+$stencil = new BitStencil();
+$stencilsList = $stencil->getList( $_REQUEST );
+$gBitSmarty->assign_by_ref( 'stencilsList', $stencilsList );
+
+// getList() has now placed all the pagination information in $_REQUEST['listInfo']
+$gBitSmarty->assign_by_ref( 'listInfo', $_REQUEST['listInfo'] );
+
+// Display the template
+$gBitSystem->display( 'bitpackage:stencil/list_stencils.tpl', tra( 'Stencil' ) , array( 'display_mode' => 'list' ));
+
 ?>
